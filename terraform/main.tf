131829476e14fcc590cd62665e4676bd04bfd2c0
Gren95 <72512054+Gren-95@users.noreply.github.com>
2024-04-05 09:54:10 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
-resource "proxmox_vm_qemu" "renee" {
-  name                    = "renee"
+resource "proxmox_vm_qemu" "efe1" {
+  name                    = "efe1"
   vmid                    = "100"
-  target_node             = "l6vi"
+  target_node             = "efe"
   clone                   = "ubuntu-server"
   cores                   = 1
   sockets                 = 1
@@ -12,7 +12,7 @@ resource "proxmox_vm_qemu" "renee" {
   scsihw                  = "virtio-scsi-pci"
   bootdisk                = "scsi0"
   os_type                 = "cloud-init"
-  cloudinit_cdrom_storage = "local-lvm"
+  cloudinit_cdrom_storage = "local-zfs"
 
   network {
     bridge = "vmbr0"
@@ -23,24 +23,24 @@ resource "proxmox_vm_qemu" "renee" {
     scsi {
       scsi0 {
         disk {
-          storage = "local-lvm"
+          storage = "local-zfs"
           size    = 50
         }
       }
     }
   }
 
-  ipconfig0 = "ip=192.168.111.71/24,gw=192.168.111.1"
+  ipconfig0 = "ip=192.168.111.131/24,gw=192.168.111.1"
   sshkeys   = <<EOF
   ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGxJ4EHJMWVBevnqdfSd4bfz5urCwu3wqXJB/RVjmwNqtQ4Oow9bxpvSxJH+y0nwXz6UA6WaayaeqPrTOnPgjYzLcKZmjwiUbr15ldNgxzZ3qgmZ/l1dfGSuo97jzIVGpat6n1ffYrKWaVw0Ov1xL4k/caIUK8Z9LGZL4TU98RJ8+9Em8hoQ1BzDDc8lIjC+lAST4R8ry2/ufsWofB7MFSrVc7rt9Br2xbCkFpYo2kldOmIT1Nbqac49uwfnEUnUN6tm6fU4C5JWicn7r0P/elkh4BPsbAiCth0lsZNzzK3riNkUKdLPB5yCYx+r1kg9txKQ9RTpONI3WCPGvA10G1c6JN3Z7rm0Rmu54762sgLjkCcbKgefaYyQ/rZdh9c7i9RxCNEngk/F8I5juht9WtHzpyIamxVx1XRhmQZxF5t7P/df1KdrnGGZlt+ZIbLtKexIQc8/GlWNn3KvBYrxjQKzuBll1icx/DeFe5yIyyu6jLSninkZVeJRt01jf/iU0= sysadmin@A326-15-2
   EOF
 
 }
 
-resource "proxmox_vm_qemu" "alex" {
-  name                    = "alex"
+resource "proxmox_vm_qemu" "efe2" {
+  name                    = "efe2"
   vmid                    = "101"
-  target_node             = "l6vi"
+  target_node             = "efe"
   clone                   = "ubuntu-server"
   count                   = 1
   cores                   = 1
@@ -52,7 +52,7 @@ resource "proxmox_vm_qemu" "alex" {
   scsihw                  = "virtio-scsi-pci"
   bootdisk                = "scsi0"
   os_type                 = "cloud-init"
-  cloudinit_cdrom_storage = "local-lvm"
+  cloudinit_cdrom_storage = "local-zfs"
 
   network {
     bridge = "vmbr0"
@@ -63,24 +63,24 @@ resource "proxmox_vm_qemu" "alex" {
     scsi {
       scsi0 {
         disk {
-          storage = "local-lvm"
+          storage = "local-zfs"
           size    = 50
         }
       }
     }
   }
 
-  ipconfig0 = "ip=192.168.111.72/24,gw=192.168.111.1"
+  ipconfig0 = "ip=192.168.111.132/24,gw=192.168.111.1"
   sshkeys   = <<EOF
   ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGxJ4EHJMWVBevnqdfSd4bfz5urCwu3wqXJB/RVjmwNqtQ4Oow9bxpvSxJH+y0nwXz6UA6WaayaeqPrTOnPgjYzLcKZmjwiUbr15ldNgxzZ3qgmZ/l1dfGSuo97jzIVGpat6n1ffYrKWaVw0Ov1xL4k/caIUK8Z9LGZL4TU98RJ8+9Em8hoQ1BzDDc8lIjC+lAST4R8ry2/ufsWofB7MFSrVc7rt9Br2xbCkFpYo2kldOmIT1Nbqac49uwfnEUnUN6tm6fU4C5JWicn7r0P/elkh4BPsbAiCth0lsZNzzK3riNkUKdLPB5yCYx+r1kg9txKQ9RTpONI3WCPGvA10G1c6JN3Z7rm0Rmu54762sgLjkCcbKgefaYyQ/rZdh9c7i9RxCNEngk/F8I5juht9WtHzpyIamxVx1XRhmQZxF5t7P/df1KdrnGGZlt+ZIbLtKexIQc8/GlWNn3KvBYrxjQKzuBll1icx/DeFe5yIyyu6jLSninkZVeJRt01jf/iU0= sysadmin@A326-15-2
   EOF
 
 }
 
-resource "proxmox_vm_qemu" "denis" {
-  name                    = "denis"
+resource "proxmox_vm_qemu" "efe3" {
+  name                    = "efe3"
   vmid                    = "102"
-  target_node             = "l6vi"
+  target_node             = "efe"
   clone                   = "ubuntu-server"
   count                   = 1
   cores                   = 1
@@ -92,7 +92,7 @@ resource "proxmox_vm_qemu" "denis" {
   scsihw                  = "virtio-scsi-pci"
   bootdisk                = "scsi0"
   os_type                 = "cloud-init"
-  cloudinit_cdrom_storage = "local-lvm"
+  cloudinit_cdrom_storage = "local-zfs"
 
   network {
     bridge = "vmbr0"
@@ -103,14 +103,14 @@ resource "proxmox_vm_qemu" "denis" {
     scsi {
       scsi0 {
         disk {
-          storage = "local-lvm"
+          storage = "local-zfs"
           size    = 50
         }
       }
     }
   }
 
-  ipconfig0 = "ip=192.168.111.73/24,gw=192.168.111.1"
+  ipconfig0 = "ip=192.168.111.133/24,gw=192.168.111.1"
   sshkeys   = <<EOF
   ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGxJ4EHJMWVBevnqdfSd4bfz5urCwu3wqXJB/RVjmwNqtQ4Oow9bxpvSxJH+y0nwXz6UA6WaayaeqPrTOnPgjYzLcKZmjwiUbr15ldNgxzZ3qgmZ/l1dfGSuo97jzIVGpat6n1ffYrKWaVw0Ov1xL4k/caIUK8Z9LGZL4TU98RJ8+9Em8hoQ1BzDDc8lIjC+lAST4R8ry2/ufsWofB7MFSrVc7rt9Br2xbCkFpYo2kldOmIT1Nbqac49uwfnEUnUN6tm6fU4C5JWicn7r0P/elkh4BPsbAiCth0lsZNzzK3riNkUKdLPB5yCYx+r1kg9txKQ9RTpONI3WCPGvA10G1c6JN3Z7rm0Rmu54762sgLjkCcbKgefaYyQ/rZdh9c7i9RxCNEngk/F8I5juht9WtHzpyIamxVx1XRhmQZxF5t7P/df1KdrnGGZlt+ZIbLtKexIQc8/GlWNn3KvBYrxjQKzuBll1icx/DeFe5yIyyu6jLSninkZVeJRt01jf/iU0= sysadmin@A326-15-2
   EOF
